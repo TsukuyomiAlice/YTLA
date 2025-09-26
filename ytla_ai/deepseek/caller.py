385c2213e41bd770f42d20fcba48282e5b213317
TsukuyomiAlice <2218907729@qq.com>
2025-09-26 15:36:23 +0800

    ai DND

--- ytla_ai/deepseek/caller.py
+++ ytla_ai/deepseek/caller.py
@@ -10,17 +10,12 @@ deepSeek_API_key = ""
 base_url = "https://api.deepseek.com"
 client = OpenAI(api_key=deepSeek_API_key, base_url=base_url)
 
-"""
-settings
-"""
-temperature = 0.0
-
-
-def chat_caller(messages, model="deepseek-chat"):
+def chat_caller(messages, model="deepseek-chat", temperature: float = 0.0):
     """
     Call the chat API
     :param messages: the messages to send
     :param model: the model to use. The alternative is "deepseek-reasoner" for using thought chain
+    :param temperature: the temperature to use
     :return: the response from the API
     """
 
